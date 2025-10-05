b3d355c2edd0a696367e37735f9fcdf0e6398ad0
puneetsharma0910 <puneetsharma0910@gmail.com>
2025-10-05 23:09:08 +0530

    initialize project with React, Vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>i have done it, i cracked offers from Amazon, Microsoft, Google....</h1>
+    
+    </>
+  )
+}
+
+export default App
