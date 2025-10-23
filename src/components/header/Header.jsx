c115c7d0187c052d7a608ce2e17856b7341fcd4e
puneetsharma0910 <puneetsharma0910@gmail.com>
2025-10-23 23:05:17 +0530

    update Container and Header components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router";
+import Container from "../container/Container";
+import Logo from '../Logo'
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
 
-export default Header
+  const navItems = [
+    { name: "Home", slug: "/", active: true },
+  { name: "Login", slug: "/login", active: !authStatus },
+  { name: "Signup", slug: "/signup", active: !authStatus },
+  { name: "All Posts", slug: "/all-posts", active: authStatus },
+  { name: "Add Post", slug: "/add-post", active: authStatus },
+  ];
+  return (
+   <>
+
+   <div>
+    <Container>
+
+      <div>
+        <Link to={'/'}>
+        <Logo width="100px"/>
+        </Link>
+      </div>
+
+      <div>
+
+        <ul>
+          {navItems.map((item)=>(
+            item.active ? (
+              <li key = {item.name}>
+
+                <button onClick={()=>navigate(item.slug)}>
+                  {item.name}
+                </button>
+
+              </li>
+            ) : null
+          ))}
+        </ul>
+      </div>
+    </Container>
+   </div>
+   
+   
+   </>
+  );
+};
+
+export default Header;
