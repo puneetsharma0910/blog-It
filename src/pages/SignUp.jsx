a9006a117a70ef13d1fb5d9b6d6e7425980d055b
puneetsharma0910 <puneetsharma0910@gmail.com>
2025-11-13 23:17:26 +0530

    Add authentication and post-creation pages

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {SignUp as SignUpComponent} from '../components/SignUp'
+
+function SignUp() {
+  return (
+    <div>
+      <SignUpComponent/>
+    </div>
+  )
+}
+
+export default SignUp
