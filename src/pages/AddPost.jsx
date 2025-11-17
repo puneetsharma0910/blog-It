6c02135a6ab7256ebe231ad11179da4108f0b825
puneetsharma0910 <puneetsharma0910@gmail.com>
2025-11-17 22:36:29 +0530

    feat(posts): add AllPosts page and update AddPost component
    - Add new src/pages/AllPosts.jsx to list and view posts
    - Update src/pages/AddPost.jsx to improve form handling and integration

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import Container from '../components/container/Container'
 import PostForm from '../components/PostForm'
 
 function AddPost() {
   return (
-    <div>
-      <PostForm/>
+    <div className='py-8'>
+        <Container>
+            <PostForm />
+        </Container>
     </div>
   )
 }
