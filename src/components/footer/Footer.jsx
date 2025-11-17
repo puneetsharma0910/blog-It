dad06c4c3dccdf0999c5c3047d4f8ca262693503
puneetsharma0910 <puneetsharma0910@gmail.com>
2025-11-17 23:33:31 +0530

    - improve accessibility (ARIA attributes) - adjust responsive layout and spacing

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Link from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import Logo from '../Logo'
 
-const Footer = () => {
+export const Footer = () => {
  return (
     <section className="relative overflow-hidden py-10 bg-gray-400 border border-t-2 border-t-black">
             <div className="relative z-10 mx-auto max-w-7xl px-4">
@@ -140,4 +140,4 @@ const Footer = () => {
   )
 }
 
-export default Footer
+export default Footer;
